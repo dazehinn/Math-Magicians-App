bcfbbb97d34e12b0d3e07b791304394cfab40e4a
dazehinn <dazehinn@yahoo.fr>
2023-04-29 19:00:13 +0100

    Add API quote component to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import APIquote from './components/APIquote';
 import Calculator from './components/calculator';
 
 function App() {
   return (
     <div className="App">
+      <APIquote />
       <Calculator />
     </div>
   );
