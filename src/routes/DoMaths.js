8916ac15a5bb187a1ff731e994b836d5acb5bd33
dazehinn <dazehinn@yahoo.fr>
2023-05-04 22:31:32 +0100

    Create component to do maths

---
+++ src/routes/DoMaths.js
@@ -0,0 +1,9 @@
+import Calculator from '../components/calculator';
+
+const DoMaths = () => (
+  <div>
+    <h2>Let&apos;s do some maths</h2>
+    <Calculator />
+  </div>
+);
+export default DoMaths;
